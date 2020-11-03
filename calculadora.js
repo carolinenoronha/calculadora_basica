053d6a6f1dc8c164e9635d1a79056819f7d67b97
Dbasilio-dev <dbasiliodev@gmail.com>
2020-11-03 12:43:17 -0300

    resolvido o bug de quanto é adicionado um número, sem uma operação previa

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,19 @@
 var inputResultado = document.getElementById("input");
 var displayValInit = "0";
 var primeiroValorRecebido = "";
+let validador = "";
 var operacaoArray = [];
 var botaoClear = document.getElementById("CE");
+let containerPrincipal = document.querySelector(".container");
+let containerTitulo = document.createElement("div");
+containerTitulo.classList.add("container-titulo");
+containerPrincipal.prepend(containerTitulo);
+let nomeCalc = document.createElement("p");
+nomeCalc.classList.add("nome-calc");
+nomeCalc.innerText = "Calculadora BR3";
+containerTitulo.appendChild(nomeCalc);
+let validaValor = false;
+
 
 // inicia tudo ao carregar a pagina
 window.addEventListener("load", function () {
@@ -11,32 +22,44 @@ window.addEventListener("load", function () {
 })
 
 //Adiciona um escutador para todos os numeros na calculadora (Precisei alterar o id de cada numero para numero0, numero1. etc)
+
 function eventoNumeros () {
     for(let i = 0; i < 10; i++){
 
         document.getElementById("numero" + [i]).addEventListener("click", displayValor)
         
     }
+
         
 }
 
 //insere o numero no display da calculadora (Precisei alterar o input para uma div comum)
 var displayValor = (eventoClick) => {
-
     var btnNumero = eventoClick.target.innerText;
+    validador = btnNumero;
+    if(validaValor == true) {
+       
+        inputResultado.innerHTML = "";
+    }
+    if(displayValInit === "0") {
+    
+        displayValInit = " ";
+    }
 
-    if(displayValInit === "0")
+    displayValInit += btnNumero;
 
-    displayValInit = " ";
+    inputResultado.innerText = displayValInit;
 
-        displayValInit += btnNumero;
-        inputResultado.innerText = displayValInit;
+    
    
 }
 
+
 // cria a funcao do botao CE no clique do usuario. O valor do display volta para o inicial "0"
 botaoClear.onclick = () => {
-    displayValInit = "0"
+    primeiroValorRecebido = "";
+    validaValor = false
+    displayValInit = "0";
     inputResultado.innerHTML = displayValInit;
 }
 
@@ -55,92 +78,87 @@ var executaOperacao = (eventoClick) => {
    var operador = eventoClick.target.innerText
 
    switch (operador){
-    case "+":
-        //O primeiro valor inserido 'e salvo na primeiroValorRecebido e o display volta a exibir 0, aguardando o segundo valor
-        primeiroValorRecebido = displayValInit;
-        displayValInit = "0";
-        inputResultado.innerHTML = displayValInit;
+        case "+":
+            
+            primeiroValorRecebido = displayValInit;
 
-        //O Array da operacao recebe o primeiro valor digitado e o operador Ex: operacaoArray = [5,"+"];
-        operacaoArray.push(primeiroValorRecebido);
-        operacaoArray.push(operador);
+            displayValInit = "0";
 
-        break;
+            inputResultado.innerHTML = displayValInit;
 
-    case "-":
-        
-         primeiroValorRecebido = displayValInit;
-         displayValInit = "0";
-         inputResultado.innerHTML = displayValInit;
- 
-         operacaoArray.push(primeiroValorRecebido);
-         operacaoArray.push(operador);
-        
-        break;
+            //O Array da operacao recebe o primeiro valor digitado e o operador Ex: operacaoArray = [5,"+"];
+            operacaoArray.push(primeiroValorRecebido);
+            operacaoArray.push(operador);
 
-    case "/":
+            break;
 
-        primeiroValorRecebido = displayValInit;
-        displayValInit = "0";
-        inputResultado.innerHTML = displayValInit;
+        case "-":
+            
+            
+            primeiroValorRecebido = displayValInit;
 
-        operacaoArray.push(primeiroValorRecebido);
-        operacaoArray.push(operador);
-        
-        break;
+            displayValInit = "0";
 
-    case "*":
-
-         primeiroValorRecebido = displayValInit;
-         displayValInit = "0";
-         inputResultado.innerHTML = displayValInit;
- 
-         operacaoArray.push(primeiroValorRecebido);
-         operacaoArray.push(operador);
+            inputResultado.innerHTML = displayValInit;
     
-    break;
+            operacaoArray.push(primeiroValorRecebido);
+            operacaoArray.push(operador);
+            
+            break;
 
-    case "=":
-        //O ultimo valor digitado pelo usuario 'e inserido no array
-        operacaoArray.push(displayValInit);
-        //O array com a operacao 'e convertido em uma string e depois pelo "eval" ele 'e analisado como uma operacao matematica
-        var calculoFinal = eval(operacaoArray.join(" "));
-        //O valor do display 'e alterado para o valor que foi recebido no calculo final
-        displayValInit = calculoFinal;
-        inputResultado.innerText = displayValInit;
-        // O valor do Array volta para o inicial
-        operacaoArray = [];
+        case "/":
+
+            
+            primeiroValorRecebido = displayValInit;
+
+            displayValInit = "0";
+            
+            inputResultado.innerHTML = displayValInit;
+
+            operacaoArray.push(primeiroValorRecebido);
+            operacaoArray.push(operador);
+            
+            break;
+
+        case "*":
+
+           
+            primeiroValorRecebido = displayValInit;
+
+            displayValInit = "0";
+
+            inputResultado.innerHTML = displayValInit;
+    
+            operacaoArray.push(primeiroValorRecebido);
+            operacaoArray.push(operador);
+        
+        break;
+
+        case "=":
+            //O ultimo valor digitado pelo usuario 'e inserido no array
+            operacaoArray.push(displayValInit);
+            //O array com a operacao 'e convertido em uma string e depois pelo "eval" ele 'e analisado como uma operacao matematica
+            var calculoFinal = eval(operacaoArray.join(" "));
+            //O valor do display 'e alterado para o valor que foi recebido no calculo final
+            displayValInit = calculoFinal;
+            inputResultado.innerText = displayValInit;
+            // O valor do Array volta para o inicial
+            operacaoArray = [];
+            validaValor = true;
+            if(validaValor) {
+                 primeiroValorRecebido = displayValInit;    
+                 displayValInit = "0";
+            }    
+            
+       
+
+        
     }
 }
+    
 
 
 
 
 
-
-
-        /*let valorRecebido1 = parseInt(prompt ("digite numero1"));
-        let valorRecebido2 = parseInt(prompt ("digite numero 2"));
-        let operadorDigitado = prompt ("digite operador");
-
-        switch (operador){
-            case "+":
-                console.log( valorRecebido1 + valorRecebido2);
-                break;
-
-            case "-":
-                console.log( valorRecebido1 - valorRecebido2);
-                break;
-
-            case "/":
-                if(valorRecebido2 != 0){
-                    console.log( valorRecebido1 / valorRecebido2);
-                } else {
-                    console.log(NaN);
-                }
-                break;
-
-            case "*":
-            console.log( valorRecebido1 * valorRecebido2);
-            
-        }*/
+        
